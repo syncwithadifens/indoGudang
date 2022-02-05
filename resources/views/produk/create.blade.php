8bbbfc4037743f53dfd1747e4de162a310ffc85a
Afiv Dicky Efendy <dickyareda@gmail.com>
2022-02-05 13:51:20 +0700

    update input form

--- resources/views/produk/create.blade.php
+++ resources/views/produk/create.blade.php
@@ -14,7 +14,7 @@ IndoGudang | Input Data Produk
             <div class="mb-3 col-lg-5">
                 <label for="nama_barang" class="form-label">Nama Barang</label>
                 <input type="text" class="bg-white form-control @error('nama_barang') is-invalid @enderror"
-                    name="nama_barang" autofocus>
+                    name="nama_barang" value="{{ old('nama_barang') }}" autofocus>
                 @error('nama_barang')
                 <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
@@ -23,8 +23,8 @@ IndoGudang | Input Data Produk
         <div class="row">
             <div class="mb-3 col-lg-5">
                 <label for="supplier" class="form-label">Supplier</label>
-                <input type="text" class="bg-white form-control @error('supplier') is-invalid @enderror"
-                    name="supplier">
+                <input type="text" class="bg-white form-control @error('supplier') is-invalid @enderror" name="supplier"
+                    value="{{ old('supplier') }}">
                 @error('supplier')
                 <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
@@ -33,7 +33,8 @@ IndoGudang | Input Data Produk
         <div class="row">
             <div class="mb-3 col-lg-5">
                 <label for="harga" class="form-label">Harga Satuan</label>
-                <input type="text" class="bg-white form-control @error('harga') is-invalid @enderror" name="harga">
+                <input type="text" class="bg-white form-control @error('harga') is-invalid @enderror" name="harga"
+                    value="{{ old('harga') }}">
                 @error('harga')
                 <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
@@ -42,7 +43,8 @@ IndoGudang | Input Data Produk
         <div class="row">
             <div class="mb-3 col-lg-5">
                 <label for="stok" class="form-label">Jumlah Stok</label>
-                <input type="text" class="bg-white form-control @error('stok') is-invalid @enderror" name="stok">
+                <input type="text" class="bg-white form-control @error('stok') is-invalid @enderror" name="stok"
+                    value="{{ old('stok') }}">
                 @error('stok')
                 <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
