b5e3fa2cf7b58dc67ecf13b8d3b6d165f48c664f
Afiv Dicky Efendy <dickyareda@gmail.com>
2022-02-15 07:11:46 +0700

    update alert-dismissible bootstrap

--- resources/views/produk/index.blade.php
+++ resources/views/produk/index.blade.php
@@ -15,7 +15,7 @@ IndoGudang | Semua Produk
 @if (session('status'))
 <div class="alert alert-success alert-dismissible fade show" role="alert">
     <strong>{{ session('status') }}!</strong>
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
     </button>
 </div>
